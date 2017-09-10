7674e83d4f0944b530b90f302d513a17734a674f
wisley <winigma@gmail.com>
2017-09-10 15:46:10 -0300

    first commit

---
+++ DesafioApps/app/src/main/java/br/com/wisley/desafioapps/MainActivity.java
@@ -0,0 +1,13 @@
+package br.com.wisley.desafioapps;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
