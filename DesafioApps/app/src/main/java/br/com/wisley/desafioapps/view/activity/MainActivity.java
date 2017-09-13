17de23a0f975af0cc76b2ab116d8ba92c7a903db
wisley <winigma@gmail.com>
2017-09-12 23:21:44 -0300

    no message

--- DesafioApps/app/src/main/java/br/com/wisley/desafioapps/view/activity/MainActivity.java
+++ DesafioApps/app/src/main/java/br/com/wisley/desafioapps/view/activity/MainActivity.java
@@ -4,12 +4,19 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import br.com.wisley.desafioapps.R;
+import br.com.wisley.desafioapps.util.ActivityUtils;
+import br.com.wisley.desafioapps.view.fragment.HomeFragment;
 
+/**
+ *
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ActivityUtils.addFragmentToActivity(getSupportFragmentManager(), new HomeFragment(), R.id.home_container);
+
     }
 }
--- DesafioApps/app/src/main/java/br/com/wisley/desafioapps/view/holder/HolderHomeFragment.java
+++ DesafioApps/app/src/main/java/br/com/wisley/desafioapps/view/holder/HolderHomeFragment.java
@@ -1,6 +1,10 @@
 package br.com.wisley.desafioapps.view.holder;
 
 import android.view.View;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import br.com.wisley.desafioapps.R;
 
 /**
  * Created by Wisley on 12/09/17.
@@ -8,17 +12,38 @@ import android.view.View;
 
 public class HolderHomeFragment extends AbstractHolderFragment {
 
+    private RelativeLayout topNews;
+    private TextView tvMainEditorial;
+    private TextView tvTittleMain;
+
     public HolderHomeFragment(View siteSelectionView) {
         super(siteSelectionView);
     }
 
+    /**
+     * Initilize all fields in view
+     */
     @Override
     protected void initializeFields() {
-
+        this.topNews = (RelativeLayout) getField(R.id.topNews);
+        this.tvMainEditorial = (TextView) getField(R.id.tvMainEditorial);
+        this.tvTittleMain = (TextView) getField(R.id.tvTittleMain);
     }
 
     @Override
     protected void initializeActions() {
 
+    }
+
+    public RelativeLayout getTopNews() {
+        return topNews;
+    }
+
+    public TextView getTvMainEditorial() {
+        return tvMainEditorial;
+    }
+
+    public TextView getTvTittleMain() {
+        return tvTittleMain;
     }
 }
